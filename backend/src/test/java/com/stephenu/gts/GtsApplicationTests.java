8398df105031a7e6ed0b86e9c874f1053aa6cee9
Susse001 <zusselman@googlemail.com>
2026-06-02 15:32:43 -0400

    Initial Spring-Boot Structure

---
+++ backend/src/test/java/com/stephenu/gts/GtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephenu.gts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
