f796e924e7c8484503ca0329b61b1cbdd493104d
Susse001 <zusselman@googlemail.com>
2026-06-03 10:28:37 -0400

    Implemented initial React frontend structure

---
+++ backend/src/main/java/com/stephenu/gts/HealthController.java
@@ -0,0 +1,13 @@
+package com.stephenu.gts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "AGTN Backend Running";
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Autonomous Galactic Trade Network</h1>
+    </>
+  );
+}
+
+export default App;
