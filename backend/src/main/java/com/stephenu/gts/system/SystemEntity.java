7ae0a5a4461f18bb9d800d0ff000ea9c681261be
Susse001 <zusselman@googlemail.com>
2026-06-03 13:21:37 -0400

    Created PostgresSQL Docker container and initial Systems table

---
+++ backend/src/main/java/com/stephenu/gts/system/SystemEntity.java
@@ -0,0 +1,32 @@
+package com.stephenu.gts.system;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "systems")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SystemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int xCoordinate;
+
+    private int yCoordinate;
+
+    private String region;
+}
