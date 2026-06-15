09b199204ad636240dfc6b13b99917b1c374ee62
Susse001 <zusselman@googlemail.com>
2026-06-15 08:54:38 -0400

    Introduced mock market and price updates to market simulation.

--- backend/src/main/java/com/stephenu/gts/simulation/SimulationService.java
+++ backend/src/main/java/com/stephenu/gts/simulation/SimulationService.java
@@ -1,34 +1,33 @@
 package com.stephenu.gts.simulation;
 
+import com.stephenu.gts.market.Market;
 import com.stephenu.gts.market.MarketRepository;
 import com.stephenu.gts.trader.TraderRepository;
+
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SimulationService {
-
+    
     private final TraderRepository traderRepository;
     private final MarketRepository marketRepository;
 
     private long currentTick = 0;
+    private final Random random = new Random();
 
+    @Transactional
     public long runTick() {
 
         currentTick++;
 
-        long traderCount = traderRepository.count();
-        long marketCount = marketRepository.count();
-
-        System.out.println(
-                "Tick "
-                + currentTick
-                + " | Traders="
-                + traderCount
-                + " Markets="
-                + marketCount
-        );
+        updateMarkets();
 
         return currentTick;
     }
@@ -36,4 +35,59 @@ public class SimulationService {
     public long getCurrentTick() {
         return currentTick;
     }
+
+    private void updateMarkets() {
+
+    List<Market> markets = marketRepository.findAll();
+
+        for (Market market : markets) {
+
+            updateSupplyAndDemand(market);
+
+            updatePrice(market);
+        }
+
+        marketRepository.saveAll(markets);
+    }
+
+    private void updateSupplyAndDemand(Market market) {
+
+        int supplyChange =
+                random.nextInt(41) - 20;
+
+        int demandChange =
+                random.nextInt(41) - 20;
+
+        market.setSupply(
+                Math.max(
+                        1,
+                        market.getSupply() + supplyChange
+                )
+        );
+
+        market.setDemand(
+                Math.max(
+                        1,
+                        market.getDemand() + demandChange
+                )
+        );
+    }
+
+    private void updatePrice(Market market) {
+
+        int price = market.getPrice();
+
+        if (market.getDemand() > market.getSupply()) {
+            price += 5;
+        }
+
+        if (market.getSupply() > market.getDemand()) {
+            price -= 5;
+        }
+
+        market.setPrice(
+                Math.max(10, price)
+        );
+    }
+
 }
