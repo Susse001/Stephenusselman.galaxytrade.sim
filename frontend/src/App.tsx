682853e656522b93287e54bba966a6832bcf2201
Susse001 <zusselman@googlemail.com>
2026-07-06 07:49:23 -0400

    Created Docker container setup for backend and frontend via nginx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 import SystemList from "./components/SystemList";
--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,5 +4,5 @@ import axios from "axios";
  * Shared Axios instance used for communicating with the backend API.
  */
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "/api"
 });
